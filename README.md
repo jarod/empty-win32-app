0265e294fba0d64fa74f4b521dce0158aa0c0e77
Jarod Liu <liuyuanzhi@gmail.com>
2015-09-19 18:10:23 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Feature
+
+* Build with CMake.
+* MinGW toolchain. Not depent on libgcc_s_dw2-1.dll, static linked.
+* Attach icon to executable(.exe).
+* Application information template.
+
---
+++ main.cc
@@ -0,0 +1,14 @@
+﻿#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance,
+				   HINSTANCE hPrevInstance,
+				   LPSTR lpCmdLine,
+				   int nCmdShow)
+{
+	MessageBoxA(nullptr,
+				"Hello World!",
+				"Empty win32 app",
+				MB_OK);
+	return 0;
+}
+
